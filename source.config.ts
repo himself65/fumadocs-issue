94a18660be2649b3e8d989d4fe1f23183ae07c8f
Alex Yang <himself65@outlook.com>
2025-03-09 03:56:23 -0700

    init repo

---
+++ source.config.ts
@@ -0,0 +1,14 @@
+import { defineDocs, defineConfig } from 'fumadocs-mdx/config';
+
+export const docs = defineDocs({
+  dir: 'content/docs',
+  docs: {
+    async: true
+  }
+});
+
+export default defineConfig({
+  mdxOptions: {
+    // MDX options
+  },
+});
